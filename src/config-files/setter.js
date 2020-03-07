92c293539962f368983f01798b1606ce8f0df733
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-07 19:01:58 +0300

    create: config-files-setter class

--- src/config-files/setter.js
+++ src/config-files/setter.js
@@ -1,15 +1,28 @@
 'use strict';
 
+const ConfigFiles = require('./config-files');
+const configFilesGetter = require('./getter');
+
 class ConfigFilesSetter {
+  constructor() {
+    this._configFiles = new ConfigFiles();
+  }
+
   setConfigFilePaths(foundPaths) {
     foundPaths.forEach((configFilePath) => {
       const configFileID = this._getConfigFileID(configFilePath);
       this._saveConfigFile(configFileID, configFilePath);
     });
+    
+    return this._configFiles;
   }
 
-  _getConfigFileID(configFilePath) {}
-  _saveConfigFile(configFileID, configFilePath) {}
+  _getConfigFileID(configFilePath) {
+    return configFilesGetter.getConfigFileID(configFilePath);
+  }
+  _setConfigFile(configFileID, configFilePath) {
+    this._configFiles.setPath(configFileID, configFilePath);
+  }
 }
 
 module.exports = ConfigFilesSetter;
