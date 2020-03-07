cafa943c3c705051bc6159a611c41c349ac97f39
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-07 19:35:57 +0300

    create: module-finder

---
+++ src/module/finder.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const path = require('path');
+const configFilesGetter = require('../config-files/getter');
+
+class ModuleFinder {
+  findModule(configFilePath, moduleName) {
+    const configFileExports = this._getConfigFileExports(configFilePath);
+    const relativeModulePath = this._getRelativeModulePath(configFileExports, moduleName);
+    const modulePath = this._createModulePath(configFilePath, relativeModulePath);
+    return this._requireModule(modulePath);
+  }
+
+  _getConfigFileExports(configFilePath) {
+    return configFilesGetter.getConfigFileExports(configFilePath);
+  }
+  _getRelativeModulePath(configFileExports, moduleName) {
+    return configFileExports[moduleName];
+  }
+  _createModulePath(configFilePath, relativeModulePath) {
+    return path.relative(configFilePath, relativeModulePath);
+  }
+  _requireModule(modulePath) {
+    return require(modulePath);
+  }
+}
+
+module.exports = ModuleFinder;
